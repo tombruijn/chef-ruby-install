ee3f20d99f0c0f838ceece5ae15cf06d77b0d4b5
Tom de Bruijn <tom@tomdebruijn.com>
2013-12-06 13:14:59 +0100

    Retrieve install location from log or fallback
    on the ruby_path variable.

--- providers/ruby.rb
+++ providers/ruby.rb
@@ -1,52 +1,63 @@
 action :install do
+  install_options = []
   ruby_implementation = new_resource.ruby_implementation
   ruby_version = new_resource.ruby_version
-  rubies_path = new_resource.rubies_path
-  ruby_path = ::File.join(rubies_path, ruby_version_string(ruby_implementation, ruby_version))
+  ruby_string = ruby_version_string(ruby_implementation, ruby_version)
 
-  execute "ruby-install[#{ruby_version_string ruby_implementation, ruby_version}]" do
+  if new_resource.rubies_path
+    ruby_path = ::File.join(new_resource.rubies_path, ruby_string)
+    install_options << "--install-dir #{ruby_path}"
+  else
+    ruby_path = ::File.join("opt", "rubies", ruby_string)
+  end
+
+  output_file = "/var/chef/cache/ruby-install-results-#{ruby_string}.log"
+  execute "ruby-install[#{ruby_string}]" do
     command <<-EOH
       /usr/local/bin/ruby-install #{ruby_implementation} #{ruby_version} \
-        --install-dir #{ruby_path}
+        #{install_options.join(" ")} > #{output_file}
     EOH
     user new_resource.user if new_resource.user
     group new_resource.group if new_resource.group
     environment new_resource.environment if new_resource.environment
 
-    action :run
+    action :nothing
     not_if { ::File.exists?(ruby_path) }
-  end
+  end.run_action(:run)
 
-  home_dir = nil
-  home_dir = "/home/#{new_resource.user}/" if new_resource.user
+  matches = `grep "Successfully installed" #{output_file}`
+    .match(/Successfully installed [^into]+ into (.*)/)
+  install_location = matches ? matches[1] : ruby_path
 
   if new_resource.update_path
+    # TODO: Add support for zsh
+    profile_file = "$HOME/.bashrc"
     source_command = "source $HOME/.ruby_path"
-    execute "Add ruby path config" do
-      # TODO: Add support for zsh
+
+    execute "Add Ruby path config" do
       command <<-EOH
-        echo >> $HOME/.bashrc;
-        echo >> $HOME/.bashrc;
-        echo #{source_command} >> $HOME/.bashrc;
+        echo >> #{profile_file};
+        echo >> #{profile_file};
+        echo #{source_command} >> #{profile_file};
       EOH
-      cwd home_dir if home_dir
       user new_resource.user if new_resource.user
       group new_resource.group if new_resource.group
 
       action :run
-      only_if { `grep "#{source_command}" $HOME/.bashrc` == "" }
+      only_if { `grep "#{source_command}" #{profile_file}` == "" }
     end
   end
 
   if new_resource.default
-    default_ruby_path = "export PATH=#{ruby_path}/bin:$PATH"
-    file "#{home_dir}.ruby_path" do
-      content default_ruby_path
+    default_ruby_path = "export PATH=#{install_location}/bin:$PATH"
+    ruby_path_file = "$HOME/.ruby_path"
+    execute "Update Ruby path" do
+      command "echo '#{default_ruby_path}' > #{ruby_path_file}"
 
       user new_resource.user if new_resource.user
       group new_resource.group if new_resource.group
 
-      not_if { `cat .ruby_path` == default_ruby_path }
+      not_if { `cat #{ruby_path_file}`.strip == default_ruby_path }
     end
   end
 end
