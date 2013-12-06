8eb872dacc1575d51b9c1ce34c98751453ada175
Tom de Bruijn <tom@tomdebruijn.com>
2013-12-06 09:39:19 +0100

    Update attributes structure

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
-default["ruby-install"]["rubies"] = {}
+default["ruby-install"]["git_url"] = ""
+default["ruby-install"]["git_ref"] = ""
+default["ruby-install"]["install_path"] = nil
+default["ruby-install"]["rubies_path"] = nil
+default["ruby-install"]["rubies"] = []
