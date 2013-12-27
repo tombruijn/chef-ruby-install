61a2c677bc14f1e4fd1dcea4ae45060af751840e
Tom de Bruijn <tom@tomdebruijn.com>
2013-12-27 13:32:29 +0100

    Update to ruby-install 0.3.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default["ruby-install"]["git_url"] = "https://github.com/postmodern/ruby-install.git"
-default["ruby-install"]["git_ref"] = "v0.3.2"
+default["ruby-install"]["git_ref"] = "v0.3.4"
 default["ruby-install"]["install_path"] = nil
 default["ruby-install"]["rubies_path"] = nil
 default["ruby-install"]["rubies"] = []
