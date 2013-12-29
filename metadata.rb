085fc238527c239d07cb74ebc82c2f49c58160d6
Tom de Bruijn <tom@tomdebruijn.com>
2013-12-29 14:25:33 +0100

    Bump version v0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "tom@tomdebruijn.com"
 license "MIT"
 description "Installs postmodern's ruby-install and optionally Ruby versions."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.0.1"
+version "0.1.0"
 
 %w{ debian ubuntu }.each do |os|
   supports os
