931509fe7d4ce0ad29350fccb516f6b5c68a95cf
Tom de Bruijn <tom@tomdebruijn.com>
2015-04-14 11:53:06 +0200

    Release version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "tom@tomdebruijn.com"
 license "MIT"
 description "Installs postmodern's ruby-install and optionally Ruby versions."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "0.1.2"
+version "0.2.0"
 
-%w{ debian ubuntu centos mac_os_x }.each do |os|
+%w(debian ubuntu centos mac_os_x).each do |os|
   supports os
 end
 
